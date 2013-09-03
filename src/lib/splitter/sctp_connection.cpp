169143fa796b1db403e36029222f3841e7ca7084
Fabian Bergmark <fabian.bergmark@gmail.com>
2013-09-03 23:57:10 +0200

    Extend SCTP notification handing

--- src/lib/splitter/sctp_connection.cpp
+++ src/lib/splitter/sctp_connection.cpp
@@ -7,6 +7,7 @@
 
 #include <memory>
 
+#include "network.hpp"
 #include "sctp_connection.hpp"
 
 SCTP::Connection::Connection(int socket)
@@ -40,7 +41,7 @@ int SCTP::Connection::recv(char *buffer, unsigned int size) {
                               , &rcvinfo, &flags);
 
   if (flags & MSG_NOTIFICATION) {
-    handle_notification(buffer);
+    handle_notification(buffer, saddr, rcvinfo);
     return 0;
   } else {
     if (recieved <= 0) {
@@ -57,7 +58,7 @@ int SCTP::Connection::get_socket() {
   return this->socket;
 }
 
-void SCTP::Connection::handle_notification(const char *buffer) {
+void SCTP::Connection::handle_notification(const char *buffer, sockaddr_in &saddr, sctp_sndrcvinfo &rcvinfo) {
 
   sctp_notification notification;
   memcpy(&notification, buffer, sizeof(sctp_notification));
@@ -66,25 +67,137 @@ void SCTP::Connection::handle_notification(const char *buffer) {
   switch (tlv.sn_type) {
   case SCTP_ASSOC_CHANGE: {
     sctp_assoc_change assoc = notification.sn_assoc_change;
+    handle_notification_assoc_change(assoc, saddr, rcvinfo);
     break;
   }
   case SCTP_PEER_ADDR_CHANGE: {
     sctp_paddr_change paddr = notification.sn_paddr_change;
+    handle_notification_paddr_change(paddr, saddr, rcvinfo);
     break;
   }
   case SCTP_REMOTE_ERROR: {
     sctp_remote_error error = notification.sn_remote_error;
+    handle_notification_remote_error(error, saddr, rcvinfo);
     break;
   }
   case SCTP_SEND_FAILED: {
     sctp_send_failed fail = notification.sn_send_failed;
+    handle_notification_send_failed(fail, saddr, rcvinfo);
     break;
   }
   case SCTP_SHUTDOWN_EVENT: {
     sctp_shutdown_event shutdown = notification.sn_shutdown_event;
+    handle_notification_shutdown(shutdown, saddr, rcvinfo);
+    break;
+  }
+  case SCTP_PARTIAL_DELIVERY_EVENT: {
+    sctp_pdapi_event pdapi = notification.sn_pdapi_event;
+    handle_notification_partial_delivery(pdapi, saddr, rcvinfo);
     break;
   }
   }
+}
+
+void SCTP::Connection::handle_notification_assoc_change(sctp_assoc_change &assoc, sockaddr_in &saddr, sctp_sndrcvinfo &rcvinfo) {
+  std::cerr << "Association change event from "
+            << ip_address((sockaddr*)&saddr);
+  switch (assoc.sac_state) {
+  case SCTP_COMM_UP:
+    std::cerr << " [SCTP_COMM_UP]"
+              << " inbound streams: (" << assoc.sac_inbound_streams << ")"
+              << " outbound streams: (" << assoc.sac_outbound_streams << ")";
+    break;
+  case SCTP_COMM_LOST:
+    std::cerr << "[SCTP_COMM_LOST]"
+              << " error: (" <<  strerror(assoc.sac_error) << ")";
+    break;
+  case SCTP_RESTART:
+    std::cerr << "[SCTP_RESTART]";
+    break;
+  case SCTP_SHUTDOWN_COMP:
+    std::cerr << "[SCTP_SHUTDOWN_COMP]";
+    break;
+  case SCTP_CANT_STR_ASSOC:
+    std::cerr << "[SCTP_CANT_STR_ASSOC]"
+              << " error: (" << strerror(assoc.sac_error) << ")";
+    break;
+  default:
+    break;
+  }
+  std::cerr << std::endl;
+}
+
+void SCTP::Connection::handle_notification_paddr_change(sctp_paddr_change &paddr, sockaddr_in &saddr, sctp_sndrcvinfo &rcvinfo) {
+  std::cerr << "Peer address change event from "
+            << ip_address((sockaddr*)&saddr);
+  switch (paddr.spc_state) {
+  case SCTP_ADDR_AVAILABLE:
+    std::cerr << "[SCTP_ADDR_AVAILABLE]";
+    break;
+  case SCTP_ADDR_UNREACHABLE:
+    std::cerr << "[SCTP_ADDR_AVAILABLE]";
+    break;
+  case SCTP_ADDR_REMOVED:
+    std::cerr << "[SCTP_ADDR_REMOVED]";
+    break;
+  case SCTP_ADDR_ADDED:
+    std::cerr << "[SCTP_ADDR_ADDED]";
+    break;
+  case SCTP_ADDR_MADE_PRIM:
+    std::cerr << "[SCTP_ADDR_MADE_PRIM]";
+    break;
+  }
+  std::cerr << " address: (" << ip_address((sockaddr*)&paddr.spc_aaddr) << ")"
+            << std::endl;
+}
+
+void SCTP::Connection::handle_notification_remote_error(sctp_remote_error &error, sockaddr_in &saddr, sctp_sndrcvinfo &rcvinfo) {
+  std::cerr << "Remote error event from "
+            << ip_address((sockaddr*)&saddr);
+  std::cerr << std::endl;
+}
+
+void SCTP::Connection::handle_notification_send_failed(sctp_send_failed &fail, sockaddr_in &saddr, sctp_sndrcvinfo &rcvinfo) {
+  std::cerr << "Send failed event to "
+            << ip_address((sockaddr*)&saddr);
+  switch (fail.ssf_flags) {
+  case SCTP_DATA_UNSENT:
+    std::cerr << "[SCTP_DATA_UNSENT]";
+    break;
+  case SCTP_DATA_SENT:
+    std::cerr << "[SCTP_DATA_SENT]";
+  }
+  std::cerr << " error: (" << fail.ssf_error << ")"
+            << std::endl;
+}
+
+void SCTP::Connection::handle_notification_shutdown(sctp_shutdown_event &shutdown, sockaddr_in &saddr, sctp_sndrcvinfo &rcvinfo) {
+  std::cerr << "Shutdown event from "
+            << ip_address((sockaddr*)&saddr);
+  std::cerr << std::endl;
+}
+
+void SCTP::Connection::handle_notification_partial_delivery(sctp_pdapi_event &pdapi, sockaddr_in &saddr, sctp_sndrcvinfo &rcvinfo) {
+  std::cerr << "Partial delivery event from "
+            << ip_address((sockaddr*)&saddr);
+  switch (pdapi.pdapi_indication) {
+  case SCTP_PARTIAL_DELIVERY_ABORTED:
+    std::cerr << "[SCTP_PARTIAL_DELIVIERY_ABORTED]";
+    break;
+  }
+  std::cerr << std::endl;
+}
+
+void SCTP::Connection::handle_notification_sender_dry(sctp_sender_dry_event &sender_dry, sockaddr_in &saddr, sctp_sndrcvinfo &rcvinfo) {
+  std::cerr << "Sender dry event from "
+            << ip_address((sockaddr*)&saddr);
+  std::cerr << std::endl;
+}
+
+void SCTP::Connection::handle_notification_stopped(sctp_notification &notifiaction, sockaddr_in &saddr, sctp_sndrcvinfo &rcvinfo) {
+  std::cerr << "Notification stopped event "
+            << ip_address((sockaddr*)&saddr);
+  std::cerr << std::endl;
 }
 
 std::shared_ptr<SCTP::Connection> SCTP::connect(const std::string &hostname, short port) {
--- src/lib/splitter/sctp_connection.hpp
+++ src/lib/splitter/sctp_connection.hpp
@@ -16,7 +16,15 @@ namespace SCTP {
     virtual int recv(char*, unsigned int);
     virtual int get_socket();
   protected:
-    virtual void handle_notification(const char*);
+    virtual void handle_notification(const char*, sockaddr_in&, sctp_sndrcvinfo&);
+    virtual void handle_notification_assoc_change(sctp_assoc_change&, sockaddr_in&, sctp_sndrcvinfo&);
+    virtual void handle_notification_paddr_change(sctp_paddr_change&, sockaddr_in&, sctp_sndrcvinfo&);
+    virtual void handle_notification_remote_error(sctp_remote_error&, sockaddr_in&, sctp_sndrcvinfo&);
+    virtual void handle_notification_send_failed(sctp_send_failed&, sockaddr_in&, sctp_sndrcvinfo&);
+    virtual void handle_notification_shutdown(sctp_shutdown_event&, sockaddr_in&, sctp_sndrcvinfo&);
+    virtual void handle_notification_partial_delivery(sctp_pdapi_event&, sockaddr_in&, sctp_sndrcvinfo&);
+    virtual void handle_notification_sender_dry(sctp_sender_dry_event&, sockaddr_in&, sctp_sndrcvinfo&);
+    virtual void handle_notification_stopped(sctp_notification&, sockaddr_in&, sctp_sndrcvinfo&);
   private:
     int socket;
   };
